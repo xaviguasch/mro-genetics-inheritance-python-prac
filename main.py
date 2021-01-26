0160c5013d87ba8c8147bf6c69119f77b426583d
xaviguasch <xaviguasch@gmail.com>
2021-01-26 11:41:13 +0100

    added finished exercise

---
+++ main.py
@@ -0,0 +1,16 @@
+class Mother:
+    def __init__(self):
+        self.eye_color = "brown"
+        self.hair_color = "dark brown"
+        self.hair_type = "curly"
+
+
+class Father:
+    def __init__(self):
+        self.eye_color = "blue"
+        self.hair_color = "blond"
+        self.hair_type = "straight"
+
+
+class Child(Mother, Father):
+    pass
